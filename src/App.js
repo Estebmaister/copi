10912a74ff004960e7f9d5b71dcdd7d11261a850
Esteban <163073317+esteban-work@users.noreply.github.com>
2024-10-20 11:16:15 -0500

    Setting root path `./` for develop

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PokeTable from './components/PokeTable';
 import PokeCard from './components/PokeCard';
 
+const basename = document.querySelector('base')?.getAttribute('href') ?? '/'
+
 function App() {
   return (
-    <Router basename="/copi">
+    <Router basename={basename}>
       <Routes>
         <Route exact path="/" element={<PokeTable />} />
         <Route path="/pokemon/:pokemonName" element={<PokeCard />} />
